c15a620b7bf1b58e39815f136b5042df30c67057
Romain Durade <durade.romain@gmail.com>
2017-06-30 20:08:35 +0200

    Modification des couleurs random

--- app/src/main/java/com/example/oriamn/tetris/utils/Rand.java
+++ app/src/main/java/com/example/oriamn/tetris/utils/Rand.java
@@ -32,12 +32,9 @@ public class Rand {
             "#f9845b", // orange
             "#838cc7", // lavender
             "#7d669e", // purple
-            "#53bbb4", // aqua
             "#51b46d", // green
             "#e0ab18", // mustard
             "#637a91", // dark gray
-            "#f092b0", // pink
-            "#b7c0c7"  // light gray
     };
 
     /**
